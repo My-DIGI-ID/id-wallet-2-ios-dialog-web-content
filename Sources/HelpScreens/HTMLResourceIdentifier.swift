ede587f95a17285c62b7f03c2bd7787f35e42650
Lars Helmuth Probst <larsprobst@ibm.com>
2022-02-22 13:09:38 +0100

    New file for identifier and swiftlint fixes.

--- Sources/HelpScreens/HTMLResourceIdentifier.swift
+++ Sources/HelpScreens/HTMLResourceIdentifier.swift
@@ -17,37 +17,36 @@ public struct HTMLResourceIdentifier: RawRepresentable, Hashable, Equatable {
     public struct HTMLResource: Hashable {
         /// Name of the HTMl Resource excluding extension
         let name: String
-        
         /// Optional sub-directory path where the resource could be located
         let subPath: String?
     }
-    
     public typealias RawValue = HTMLResource
     public let rawValue: RawValue
-    
     public init(rawValue: RawValue) {
         self.rawValue = rawValue
     }
 }
 
- public extension HTMLResourceIdentifier {
+public extension HTMLResourceIdentifier {
+     /// The path to the html file
     var url: URL? {
         guard let language = Locale.current.languageCode else { return nil }
         return url(for: language)
     }
-    
+    /// Returns the url for the html file
+    /// - Parameter locale: users locale (for example en)
+    /// - Returns: the file url to the html file
     func url(for locale: String) -> URL? {
         let localizedSubPath = rawValue.subPath.map { "\(locale)/\($0)"} ?? locale
-        return Bundle.module.url(forResource: rawValue.name, withExtension: "html", subdirectory: "html/\(localizedSubPath)")
+        return Bundle.module.url(
+            forResource: rawValue.name,
+            withExtension: "html",
+            subdirectory: "html/\(localizedSubPath)")
     }
-    
     /// Convenience initializer that omits th rawValue identifier
+    /// - Parameter name: the name of the html file without extension
+    /// - Parameter subPath: specify a sub path if there is one
     init(name: String, subPath: String? = nil) {
         self.init(rawValue: .init(name: name, subPath: subPath))
     }
 }
-
-public extension HTMLResourceIdentifier {
-    static public let learnMore = HTMLResourceIdentifier(name: "learnMore")
-}
-
